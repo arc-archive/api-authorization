4944a28f62b3cfc5e84a742281c75acdb683c0a8
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2020-12-15 17:14:42 -0300

    test: add api-view-model-trasnformer import to karma

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
 const { createDefaultConfig } = require('@open-wc/testing-karma');
 const merge = require('deepmerge');
 
-module.exports = config => {
+module.exports = (config) => {
   config.set(
     merge(createDefaultConfig(config), {
       files: [
@@ -13,36 +13,40 @@ module.exports = config => {
         // npm run test -- --grep test/bar/*
         { pattern: config.grep ? config.grep : 'test/**/*.test.js', type: 'module' },
         {
-            pattern: 'node_modules/cryptojslib/components/core.js',
-            type: 'js'
-          },
-          {
-            pattern: 'node_modules/cryptojslib/rollups/sha1.js',
-            type: 'js'
-          },
-          {
-            pattern: 'node_modules/cryptojslib/components/enc-base64-min.js',
-            type: 'js'
-          },
-          {
-            pattern: 'node_modules/cryptojslib/rollups/md5.js',
-            type: 'js'
-          },
-          {
-            pattern: 'node_modules/cryptojslib/rollups/hmac-sha1.js',
-            type: 'js'
-          }
+          pattern: 'node_modules/cryptojslib/components/core.js',
+          type: 'js'
+        },
+        {
+          pattern: 'node_modules/cryptojslib/rollups/sha1.js',
+          type: 'js'
+        },
+        {
+          pattern: 'node_modules/cryptojslib/components/enc-base64-min.js',
+          type: 'js'
+        },
+        {
+          pattern: 'node_modules/cryptojslib/rollups/md5.js',
+          type: 'js'
+        },
+        {
+          pattern: 'node_modules/cryptojslib/rollups/hmac-sha1.js',
+          type: 'js'
+        },
+        {
+          pattern: 'node_modules/@api-components/api-view-model-transformer/api-view-model-transformer.js',
+          type: 'module'
+        }
       ],
 
       esm: {
-        nodeResolve: true,
+        nodeResolve: true
       },
       client: {
         mocha: {
           timeout: 10000
         }
-      },
-    }),
+      }
+    })
   );
   return config;
 };
